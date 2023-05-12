743ff8412dfbaf7d39d9db347939691fc0c4d5b6
Praneeth Yerrapragada <praneeth.yerrapragada@gritfinancial.org>
2023-05-12 10:15:00 -0400

    style: format main.py

--- src/main.py
+++ src/main.py
@@ -7,25 +7,27 @@ if __name__ == '__main__':
     app = App()
     llm = LLM()
 
-    loader = CSVLoader(file_path='/Users/praneeth/Documents/ml/marketing-chatbot/data/pricing.csv', encoding="utf-8", csv_args={
-                'delimiter': ','})
+    loader = CSVLoader(file_path='/Users/praneeth/Documents/ml/marketing-chatbot/data/pricing.csv',
+                       encoding="utf-8", 
+                       csv_args={'delimiter': ','})
     data = loader.load()
     print(f'data: {data}')
 
     conversation_retrieval_chain = llm.init_conversation_retrieval_chain(data)
     conversation_chain = llm.init_conversation_chain()
-    
+
     input = app.get_user_input()
 
     if input:
         # output = conversation_chain.predict(input=input)
         # print(f'conversation_chain output: {output}')
 
-        output = conversation_retrieval_chain(inputs={"question": input, "chat_history": app.get_history()}, 
-                                               return_only_outputs=True)
+        output = conversation_retrieval_chain(inputs={"question": input, 
+                                                      "chat_history": app.get_history()},
+                                              return_only_outputs=True)
 
         print(f'conversation_retrieval_chain output: {output}')
         app.set_generated_message_state(output['answer'])
         app.set_history(input, output['answer'])
-    
+
     app.create_chat_component()
